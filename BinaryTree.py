c6291bbee3c38bc599b8a81755d44fe31444602c
pynaruer <62301358+pynaruer@users.noreply.github.com>
2020-04-08 16:43:59 +0200

    q7

--- BinaryTree.py
+++ BinaryTree.py
@@ -1,19 +1,26 @@
 from Node import *
 
-class BinaryTree :
+class BinaryTree() :
     def __init__(self,root):
         self.__root = root
+
     def getRoot(self):
         return self.__root
 
-Node3 = Node(3,None,None)
-Node4 = Node(4,Node3,None)
-Node6 = Node(6,None,None)
-Node5 = Node(5,Node4,Node6)
+    def isRoot(self, node):
+        if self.__root == node :
+            return True
+        else :
+            return False
 
-Node18 = Node(18,None,None)
-Node21 = Node(21,None,None)
-Node19 = Node(19,Node18,Node21)
-Node17 = Node(17,None,Node19)
 
-Node12 = Node(12,Node5,Node17)
+arbre = BinaryTree(Node(12,None,None))
+arbre.getRoot().setLeft(Node(5,None,None))
+arbre.getRoot().getLeft().setLeft(Node(4,None,None))
+arbre.getRoot().getLeft().setRight(Node(6,None,None))
+arbre.getRoot().getLeft().getLeft().setLeft(Node(3,None,None))
+
+arbre.getRoot().setRight(Node(17,None,None))
+arbre.getRoot().getRight().setRight(Node(19,None,None))
+arbre.getRoot().getRight().getRight().setLeft(Node(18,None,None))
+arbre.getRoot().getRight().getRight().setRight(Node(21,None,None))

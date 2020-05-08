bd1934955790882e553e4a688c594e7d6515ef34
pynaruer <62301358+pynaruer@users.noreply.github.com>
2020-05-08 19:47:08 +0200

    final

--- BinaryTree.py
+++ BinaryTree.py
@@ -1,5 +1,6 @@
 from Node import *
 
+
 #DEF FONCTIONS
 
 class BinaryTree():
@@ -63,6 +64,63 @@ class BinaryTree():
         else:
             return self.belongs(node.getRight(), val) or self.belongs(node.getLeft(), val)
 
+    def ancestors(self, node, val):
+        if node == None :
+            return (val,"n'est pas dans cette arbre")
+        else:
+            if self.belongs(node.getLeft(), val):
+                return str(node.getVal())+" "+str(self.ancestors(node.getLeft(), val))
+            elif self.belongs(node.getRight(), val):
+                return str(node.getVal())+" "+str(self.ancestors(node.getRight(), val))
+
+    def descendants(self, node, val):
+        if node == None:
+            return (val,"n'est pas dans cette arbre")
+        elif node.getVal() == val:
+            return self.printValues(node.getLeft())+self.printValues(node.getRight())
+        else:
+            if self.belongs(node.getLeft(), val):
+                return self.descendants(node.getLeft(), val)
+            elif self.belongs(node.getRight(), val):
+                return self.descendants(node.getRight(), val)
+
+    def prefixe(self, node):
+        if node == None:
+            return ""
+        else:
+            return str(node.getVal())+" "+str(self.prefixe(node.getLeft()))+str(self.prefixe(node.getRight()))
+
+    def infixe(self, node):
+        if node == None:
+            return ""
+        else:
+            return str(self.infixe(node.getLeft()))+str(node.getVal())+" "+str(self.infixe(node.getRight()))
+
+    def postfixe(self, node):
+        if node == None:
+            return ""
+        else:
+            return str(self.postfixe(node.getLeft()))+str(self.postfixe(node.getRight()))+str(node.getVal()) + " "
+
+    def listeTree(self, node):
+        if node == None:
+            return ""
+        elif node.getLeft() == None and node.getRight() == None:
+            return [node.getVal()]
+        else:
+            return [node.getVal(), (self.listeTree(node.getLeft())), self.listeTree(node.getRight())]
+
+
+    def parcoursLargeur(self, node):
+        list = self.listeTree(node)
+        for elt in list:
+            temp = ""
+            if type(elt) == type(1):
+                temp = temp+str(elt)+" "
+            elif elt != None:
+                for i in elt:
+                    list.append(i)
+        return temp
 
 
 #PRINCIPAL
@@ -86,4 +144,9 @@ print("number leaves :",tree.numberLeaves(tree.getRoot()))
 print("number internal nodes:",tree.numberInternalNodes(tree.getRoot()))
 print("height :",tree.height(tree.getRoot()))
 val = 1
-print("belong :",val," ?",tree.belongs(tree.getRoot(),val))
+print("belong :",val," :",tree.belongs(tree.getRoot(),val))
+print("ancestors ",val," : ", tree.ancestors(tree.getRoot(), val))
+print("préfixe :", tree.prefixe(tree.getRoot()))
+print("infixe :", tree.infixe(tree.getRoot()))
+print("postfixe :", tree.postfixe(tree.getRoot()))
+print("parcoursLargeur :", tree.parcoursLargeur(tree.getRoot()))

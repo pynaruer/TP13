26c1c1af4113ec82803797c83e2ef0e07f3602c1
pynaruer <62301358+pynaruer@users.noreply.github.com>
2020-05-08 20:23:06 +0200

    final def classe

---
+++ BinarySearchTree.py
@@ -0,0 +1,24 @@
+from BinaryTree import *
+from Node import *
+
+#DEF FONCTIONS
+class BinarySearchTree(BinaryTree):
+
+    def __init__(self, root):
+        BinaryTree.__init__(self, root)
+
+    def contains(self, node, value):
+        if node is None:
+            return 0
+        elif node.getVal() == value and self.isRoot(node):
+            return True
+        elif node.getVal() == value:
+            return 1
+        else:
+            return (self.contains(node.getLeft(), value)+self.contains(node.getRight(), value)) > 0
+
+    def findMin(self, node):
+        return min((self.infixe(node)).split())
+
+    def findMax(self, node):
+        return max((self.infixe(node)).split())
